67d2cf826c97ab8aa7b6dad4beb6a9c60e526187
EC2 Default User <112703710+mihocororo@users.noreply.github.com>
2022-09-20 15:13:31 +0000

    kansei

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+
+  end
+
+  def create
+    @books = Book.all.order(id: :asc)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    book.save
+      flash[:notice] = 'Book was successfully destroyed.'
+    redirect_to '/books'
+  end
+
+
+
+
+  def edit
+     @book = Book.find(params[:id])
+
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+    flash[:notice] = 'Book was successfully updated.'
+    redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
